188176fbc7fcc45ae106318b4a2a51bd9adf5bbf
jdoran <justin.doran2002@gmail.com>
2023-09-10 18:25:59 -0400

    4 added

--- src/Vector.java
+++ src/Vector.java
@@ -1,44 +1,24 @@
 public class Vector {
     String[] array;
 
-    public Vector() {
-        this.array = new String[0];
+    public Vector(String[] array) {
+        this.array = array;
     }
 
-    public void grow(String val) {
-        String[] newArray = new String[this.array.length + 1];
-
-        for (int i = 0; i < this.array.length; i++) {
-            newArray[i] = this.array[i];
+    public void Modify(String[] row) {
+        for (int i = 0; i < row.length; i++) {
+            this.array[i] = row[i];
         }
-
-        newArray[newArray.length - 1] = val;
-        this.array = newArray;
     }
 
-    public int findIndex(String val) {
-        String elem;
-
-        for (int i = 0; i < this.array.length; i++) {
-            elem = this.array[i];
-            if (elem.equals(val)) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    public void getCourse(int courseIndex, int numHoles, String section) {
+    public void getPars(int numHoles, String section) {
         int start;
         String[] newArray = new String[numHoles];
 
-        if (numHoles == 18) {
-            start = courseIndex * 18;
-        } else if (section == "front") {
-            start = courseIndex * 18;
+        if (section.equals("back")) {
+            start = 10;
         } else {
-            start = (courseIndex * 18) + 9;
+            start = 1;
         }
 
         for (int i = 0; i < numHoles; i++) {
